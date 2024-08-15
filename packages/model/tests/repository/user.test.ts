db5bbf25c3efdd0118bff6cb88f77df12f6a3a0c
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-08-14 22:00:00 -0400

    getCurrentUser testing

--- packages/model/tests/repository/user.test.ts
+++ packages/model/tests/repository/user.test.ts
@@ -1,5 +1,49 @@
-import { expect, test } from "vitest";
+import { expect, describe, afterEach, vi, it, beforeEach } from "vitest";
+import { getCurrentUser } from "../../repository/user";
+import prisma from "../../prisma/prisma-client";
 
-test("First test", () => {
-  expect(2).toBe(2);
+const mocksGet = vi.hoisted(() => ({
+  get: vi.fn(() => ({ value: "" })),
+}));
+vi.mock("next/headers", () => ({
+  cookies: () => ({
+    get: mocksGet.get,
+  }),
+}));
+
+describe("User", () => {
+  let userId;
+
+  beforeEach(async () => {
+    userId = (
+      await prisma.user.create({
+        data: {
+          name: "test",
+          phone: "test",
+        },
+      })
+    ).id;
+  });
+
+  afterEach(async () => {
+    vi.clearAllMocks();
+    await prisma.user.deleteMany();
+  });
+
+  it("First test", async () => {
+    const user = await getCurrentUser();
+    expect(user).toBeNull();
+  });
+
+  it("The userId is not in the db", async () => {
+    mocksGet.get.mockReturnValue({ value: "id-not-in-db" });
+    const user = await getCurrentUser();
+    expect(user).toBeNull();
+  });
+
+  it("The userId is in the db", async () => {
+    mocksGet.get.mockReturnValue({ value: userId });
+    const user = await getCurrentUser();
+    expect(user?.id).equals(userId);
+  });
 });
---
+++ packages/model/tests/vitest.setup.ts
@@ -0,0 +1,2 @@
+process.env.DATABASE_URL =
+  "postgresql://postgres:postgres@localhost:5432/mydb-testing?schema=public";
