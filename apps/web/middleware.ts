9e94666466f3d0a6a56752486a7ff62a26b1bab3
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-09-14 14:40:41 -0400

    Remove locale en

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ["en", "es"],
+  locales: ["es"],
 
   // Used when no locale matches
   defaultLocale: "es",
