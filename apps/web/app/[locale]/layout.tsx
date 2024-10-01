eb6ecb260b460c596b922e948fb0fbe7397c5aaa
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-09-30 21:37:42 -0400

    Change the ico

--- apps/web/app/[locale]/layout.tsx
+++ apps/web/app/[locale]/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({
           <NextIntlClientProvider messages={messages}>
             {business ? (
               <>
-                <Header business={business} locale={locale} />
+                <Header business={business} locale={locale} logo="/logo.jpg" />
                 <main className="flex-1 container pt-20 md:py-16 lg:py-20">
                   {children}
                 </main>
