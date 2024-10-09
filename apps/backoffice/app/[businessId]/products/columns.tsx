0e36250c74d5e9a242e6c623b30cf2c7d51d50dd
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-10-09 16:22:08 -0400

    In the product table the user will see the priority

--- apps/backoffice/app/[businessId]/products/columns.tsx
+++ apps/backoffice/app/[businessId]/products/columns.tsx
@@ -4,21 +4,39 @@ import Link from "next/link";
 
 export const columns: ColumnDef<any>[] = [
   {
-    header: "Nombre",
-    accessorKey: "name",
-    row: ({ cell: { value, row } }: { cell: { value: string; row: any } }) => {
+    header: "Image",
+    accessorKey: "image",
+    cell: ({ cell: { value, row } }: { cell: { value: string; row: any } }) => {
       return (
-        <div className="flex flex-1">
+        <Link
+          href={`products/${row.id}`}
+          className="bg-white hover:bg-gray-100 text-gray-800 h-10 w-10 text-blue-500 underline hover:text-blue-700 transition-colors"
+          aria-label="View"
+        >
           <Image src={row.image} width={48} height={48} alt={row.name} />
-          <Link
-            href={`products/${row.id}`}
-            className="flex-1 bg-white hover:bg-gray-100 text-gray-800 h-10 w-10 text-blue-500 underline hover:text-blue-700 transition-colors"
-            aria-label="View"
-          >
-            {value}
-          </Link>
-        </div>
+        </Link>
       );
     },
+  },
+  {
+    header: "Nombre",
+    accessorKey: "name",
+    cell: ({ cell: { value, row } }: { cell: { value: string; row: any } }) => {
+      return (
+        <Link
+          href={`products/${row.id}`}
+          className="bg-white hover:bg-gray-100 text-gray-800 h-10 w-10 text-blue-500 underline hover:text-blue-700 transition-colors"
+          aria-label="View"
+        >
+          {value}
+        </Link>
+      );
+    },
+  },
+  {
+    header: "Prioridad",
+    accessorKey: "priority",
+    cell: ({ cell: { value, row } }: { cell: { value: string; row: any } }) =>
+      value,
   },
 ];
