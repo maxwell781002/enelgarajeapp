742cce9600a83f699c0057a3a822163fb64338d4
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-10-10 15:15:14 -0400

    Filter category

--- apps/backoffice/app/[businessId]/categories/page.tsx
+++ apps/backoffice/app/[businessId]/categories/page.tsx
@@ -2,15 +2,12 @@ import { crud } from "@repo/model/lib/crud";
 import { CategoryRepository } from "@repo/model/repositories/category";
 import CategoryTable from "./table";
 import { TableContextProvider } from "@repo/ui/context/table";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@repo/ui/components/ui/card";
 import { DialogForm } from "./DialogForm";
 import { getTranslations } from "next-intl/server";
 import { PaginationResult } from "@repo/model/types/pagination";
+import TableLayout from "@repo/ui/components/table-layout";
+import Filter from "./filters";
+import { redirect } from "next/navigation";
 
 type PageProps = {
   searchParams: any;
@@ -22,33 +19,55 @@ export default async function Page({
   params: { businessId },
 }: PageProps) {
   const t = await getTranslations("Category");
-  const { list, remove, update, create } = crud(
+  const { list, remove, update, create, search } = crud(
     `/${businessId}/categories`,
     CategoryRepository.name,
     searchParams,
   );
+  const handleSearch = async (query: any) => {
+    "use server";
+    const url = await search(query);
+    return redirect(url);
+  };
   const pagination = await list({ ...searchParams, businessId });
   return (
-    <Card>
-      <CardHeader className="px-7">
-        <div className="flex flex-1">
-          <div>
-            <CardTitle>{t("CategoryList")}</CardTitle>
-          </div>
-          <div className="flex-1 flex justify-end">
-            <DialogForm
-              title={t("createCategory")}
-              action={create}
-              defaultValues={{ businessId, active: true, priority: 0 }}
-            />
-          </div>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <TableContextProvider update={update} remove={remove}>
-          <CategoryTable pagination={pagination as PaginationResult<any>} />
-        </TableContextProvider>
-      </CardContent>
-    </Card>
+    <TableLayout
+      title={t("CategoryList")}
+      filter={<Filter onChange={handleSearch} />}
+      buttons={
+        <DialogForm
+          title={t("createCategory")}
+          action={create}
+          defaultValues={{ businessId, active: true, priority: 0 }}
+        />
+      }
+    >
+      <TableContextProvider update={update} remove={remove}>
+        <CategoryTable pagination={pagination as PaginationResult<any>} />
+      </TableContextProvider>
+    </TableLayout>
   );
+  // return (
+  //   <Card>
+  //     <CardHeader className="px-7">
+  //       <div className="flex flex-1">
+  //         <div>
+  //           <CardTitle>{t("CategoryList")}</CardTitle>
+  //         </div>
+  //         <div className="flex-1 flex justify-end">
+  //           <DialogForm
+  //             title={t("createCategory")}
+  //             action={create}
+  //             defaultValues={{ businessId, active: true, priority: 0 }}
+  //           />
+  //         </div>
+  //       </div>
+  //     </CardHeader>
+  //     <CardContent>
+  //       <TableContextProvider update={update} remove={remove}>
+  //         <CategoryTable pagination={pagination as PaginationResult<any>} />
+  //       </TableContextProvider>
+  //     </CardContent>
+  //   </Card>
+  // );
 }
