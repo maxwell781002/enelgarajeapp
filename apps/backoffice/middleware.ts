a7e83cc1f038e8f6aa3ef382e4746e7d15e3a496
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-10-21 16:45:18 -0400

    Add businessId to the user

--- apps/backoffice/middleware.ts
+++ apps/backoffice/middleware.ts
@@ -1,10 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@repo/model/lib/auth";
+import { UserRoles } from "@repo/model/repositories/user";
 
 export const runtime = "experimental-edge";
 
 export default async function middleware(request: NextRequest) {
   const session = await auth();
+  
+  console.log('middleware', session?.user);
+  if (session?.user?.role === UserRoles.USER) {
+    
+  }
+
   const isLogin = request.nextUrl.pathname === "/login";
   if (!session && !isLogin) {
     return NextResponse.redirect(new URL("/login", request.url));
--- packages/model/lib/auth.ts
+++ packages/model/lib/auth.ts
@@ -2,10 +2,19 @@ import NextAuth, { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@repo/model/prisma/prisma-client";
+import { businessRepository } from "../repositories/business";
+
+const adapter: any = PrismaAdapter(prisma);
+const getUserByAccount = async (provider_providerAccountId: any) => {
+  const user = await adapter.getUserByAccount(provider_providerAccountId)
+  const businessIds = user?.id ? await businessRepository.getBusinessIdByUser(user.id) : [];
+  user.businessIds = businessIds
+  return user
+}
 
 const config = {
   providers: [Google],
-  adapter: PrismaAdapter(prisma),
+  adapter: { ...adapter, getUserByAccount },
   session: { strategy: "jwt" },
   callbacks: {
     async session({ session, token }: any) {
