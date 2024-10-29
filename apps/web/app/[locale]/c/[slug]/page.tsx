722fba7121c0e77fc1952d3e62df62b833f121f9
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-10-29 07:20:20 -0400

    In the frontend does not show the categories if the business not can create them.

--- apps/web/app/[locale]/c/[slug]/page.tsx
+++ apps/web/app/[locale]/c/[slug]/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
       currentBusiness={currentBusiness}
       categorySlug={slug}
       baseUrl={`/c/${slug}`}
-      businessId={currentBusiness.id}
+      business={currentBusiness}
       searchParams={searchParams}
     />
   );
