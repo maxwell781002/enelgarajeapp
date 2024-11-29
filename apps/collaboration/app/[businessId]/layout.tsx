81c9e72df6230706d404100e322ed8169fadc7a8
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-11-29 13:57:07 -0500

    button in the user menu

--- apps/collaboration/app/[businessId]/layout.tsx
+++ apps/collaboration/app/[businessId]/layout.tsx
@@ -6,8 +6,6 @@ import { auth } from "@repo/model/lib/auth";
 import { businessMenu } from "../config/menu";
 import ShoppingCartHeader from "@repo/ui/components/shop-cart/shopping-cart-header";
 import { getCurrentOrder } from "@repo/model/repository/order";
-import SwitchApp from "@repo/ui/components/switch-app";
-import { ApplicationsNames } from "@repo/model/lib/applications-names";
 import { getBusinessSecurity } from "@repo/model/repository/user";
 import { redirect } from "next/navigation";
 import { UserBusinessType } from "@repo/model/repositories/business";
@@ -44,21 +42,15 @@ export default async function RootLayout({
         businessId={businessId}
         business={business as Item[]}
         onChangeBusiness={onChangeBusiness}
+        switchApp
         headerExtra={
-          <>
-            <div className="flex flex-1 justify-end items-center">
-              <SwitchApp
-                application={ApplicationsNames.COLLABORATOR}
-                className="mr-4"
-                businessId={businessId}
-              />
-              <ShoppingCartHeader
-                className="mr-4"
-                order={order}
-                url={`/${businessId}/shopping-cart`}
-              />
-            </div>
-          </>
+          <div className="flex flex-1 justify-end items-center">
+            <ShoppingCartHeader
+              className="mr-4"
+              order={order}
+              url={`/${businessId}/shopping-cart`}
+            />
+          </div>
         }
       >
         {children}
