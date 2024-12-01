36f8e56e098223e970a5e95fc3f11015b93af1cd
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-12-01 13:50:05 -0500

    Empty table

--- apps/collaboration/app/[businessId]/products/table.tsx
+++ apps/collaboration/app/[businessId]/products/table.tsx
@@ -2,6 +2,7 @@
 import { PaginationResult } from "@repo/model/types/pagination";
 import { IProduct } from "@repo/model/types/product";
 import { CardItem } from "@repo/ui/components/cardList/card";
+import EmptyTable from "@repo/ui/components/table/empty";
 import Pagination from "@repo/ui/components/table/pagination";
 
 export default function ProductTable({
@@ -13,6 +14,14 @@ export default function ProductTable({
   add: (productId: string) => void;
   baseUrl: string;
 }) {
+  if (data.length === 0) {
+    return (
+      <EmptyTable
+        emptyTitle="No hay productos"
+        emptyDescription="No hay productos disponibles o no hay para los filtros seleccionados."
+      />
+    );
+  }
   return (
     <div>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
