d0b62a40ec451f261f9aeec677a730aea2866290
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-12-23 13:57:10 -0500

    Collaborator checkout form

--- apps/collaboration/app/[businessId]/(shop-cart)/checkout/page.tsx
+++ apps/collaboration/app/[businessId]/(shop-cart)/checkout/page.tsx
@@ -1,5 +1,6 @@
 import { getBusinessById } from "@repo/model/repository/business";
 import CheckoutPage from "@repo/ui/components/shop-cart/checkout/index";
+import CheckoutForm from "./form";
 
 export type Props = {
   params: {
@@ -9,14 +10,19 @@ export type Props = {
 
 export default async function Component({ params: { businessId } }: Props) {
   const business = await getBusinessById(businessId);
+  const action = async (data: any) => {
+    "use server";
+    console.log(data);
+  };
   return (
-    <CheckoutPage
-      business={business}
-      baseUrl={`/${businessId}`}
-      isCollaborator
-      addAliasToAddress={false}
-      addressUrl={`/${businessId}/address-user`}
-      productBaseUrl={`/${businessId}/products`}
-    />
+    <CheckoutPage>
+      <CheckoutForm
+        business={business}
+        action={action}
+        defaultValues={{
+          wantDomicile: true,
+        }}
+      />
+    </CheckoutPage>
   );
 }
