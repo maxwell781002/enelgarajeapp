372cc279ce4d1816a0cab4be8a4e5fcd98c50653
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2025-01-04 17:13:52 -0500

    fix error

--- packages/model/api/shop-cart/route.ts
+++ packages/model/api/shop-cart/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSearchParams } from "@repo/model/lib/url";
 import { orderItems } from "../../repository/checkout";
+import { BadRequestError } from "../../errors/bad-request";
 
 export async function POST(req: NextRequest) {
   const params: any = getSearchParams(req.nextUrl.searchParams);
@@ -9,10 +10,17 @@ export async function POST(req: NextRequest) {
   }
   const body = await req.json();
   const isCollaborator = params.isCollaborator === "true";
-  const { hasProductOutOfStock } = await orderItems(
-    params.businessId,
-    body,
-    isCollaborator,
-  );
-  return NextResponse.json({ hasProductOutOfStock });
+  try {
+    const { hasProductOutOfStock } = await orderItems(
+      params.businessId,
+      body,
+      isCollaborator,
+    );
+    return NextResponse.json({ hasProductOutOfStock });
+  } catch (error: any) {
+    if (error.name === BadRequestError.name) {
+      return NextResponse.json({ message: error.message });
+    }
+    throw error;
+  }
 }
