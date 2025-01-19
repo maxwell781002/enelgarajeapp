1b7ddff3f0204cde4be0335b9b859b89d21fd0f6
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2025-01-18 21:49:51 -0500

    configure database test

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,8 +4,8 @@
 //   "postgresql://catalog_local_owner:zG3K7etnDZYT@ep-shy-pine-a5ouqatg.us-east-2.aws.neon.tech/catalog_testing?sslmode=require";
 
 process.env.POSTGRES_PRISMA_URL =
-  "postgres://postgres:postgres@localhost:5432/main";
+  "postgres://postgres:postgres@localhost:5432/main-test";
 process.env.POSTGRES_URL_NON_POOLING =
-  "postgres://postgres:postgres@localhost:5432/main";
+  "postgres://postgres:postgres@localhost:5432/main-test";
 
 process.env.BOT_WEBHOOK_URL = "";
