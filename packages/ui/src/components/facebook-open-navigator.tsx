5dab2c49188ef7c1bd59a3b2fd2191f9b3dadf04
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2025-02-19 20:13:43 -0500

    remove redirect

--- packages/ui/src/components/facebook-open-navigator.tsx
+++ packages/ui/src/components/facebook-open-navigator.tsx
@@ -8,12 +8,14 @@ export type FacebookOpenNavigatorProps = {
 
 export default function FacebookOpenNavigator({ children }: FacebookOpenNavigatorProps) {
   useEffect(() => {
+    console.log("FacebookOpenNavigator");
     if (typeof window !== 'undefined') {
-      const userAgent = window.navigator.userAgent;
-      if (userAgent.indexOf("FBAN") !== -1 || userAgent.indexOf("FBAV") !== -1) {
-        const currentDomain = window.location.origin;
-        window.location.href = currentDomain;
-      }
+      console.log(window.navigator.userAgent);
+      // const userAgent = window.navigator.userAgent;
+      // if (userAgent.indexOf("FBAN") !== -1 || userAgent.indexOf("FBAV") !== -1) {
+      //   const currentDomain = window.location.origin;
+      //   window.location.href = currentDomain;
+      // }
     }
   }, []);
 
