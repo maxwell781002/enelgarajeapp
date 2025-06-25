1f2b4f79d29b178e98053409b19cbc14cc217462
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2025-06-25 10:09:45 -0400

    Image loader

--- packages/ui/src/components/image.tsx
+++ packages/ui/src/components/image.tsx
@@ -1,5 +1,36 @@
-import BaseImage from "next/image";
+import BaseImage, { ImageProps as NextImageProps } from "next/image";
 
-export default function Image({ src, ...props }: any) {
-  return <BaseImage src={src.downloadUrl} {...props} />;
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#555" offset="20%" />
+      <stop stop-color="#333" offset="50%" />
+      <stop stop-color="#555" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#555" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+
+type ImageProps = {
+  src: any;
+} & NextImageProps;
+
+export default function Image({ src, height, width, ...props }: ImageProps) {
+  return (
+    <BaseImage
+      placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(width as number, height as number))}`}
+      src={src.downloadUrl}
+      height={height}
+      width={width}
+      {...props}
+    />
+  );
 }
