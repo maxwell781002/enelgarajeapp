35118ce970a9a8df27db4cd504e0bc0ab8e52b81
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2025-07-14 19:51:12 -0400

    Show or hide the business profit

--- apps/backoffice/app/[businessId]/products/form/prices.tsx
+++ apps/backoffice/app/[businessId]/products/form/prices.tsx
@@ -3,6 +3,7 @@ import Commissions, {
 } from "@repo/ui/components/prices/commissions";
 import PriceWidget from "@repo/ui/components/prices/price-widget";
 import { commissionCalculate } from "@repo/model/lib/utils";
+import { useState } from "react";
 
 export type PriceProps = {
   form: any;
@@ -12,6 +13,8 @@ export default function Price({ form, ...props }: PriceProps) {
   const price = form.watch("price");
   const offerPrice = form.watch("offerPrice");
   const basePrice = offerPrice && offerPrice < price ? offerPrice : price;
+  const [businessProfitSwitch, setBusinessProfitSwitch] = useState(false);
+  const [customBusinessProfit, setCustomBusinessProfit] = useState(0);
   const [commission, businessProfit] = commissionCalculate(
     basePrice,
     form.watch("priceValues.commissionType"),
@@ -20,7 +23,14 @@ export default function Price({ form, ...props }: PriceProps) {
   const showCommission = form.watch("priceValues.hasCommission");
   return (
     <>
-      <PriceWidget form={form} showCommission={showCommission} />
+      <PriceWidget
+        form={form}
+        showCommission={showCommission}
+        changeBusinessProfit={setBusinessProfitSwitch}
+        showBusinessProfit={businessProfitSwitch}
+        customBusinessProfit={customBusinessProfit}
+        setCustomBusinessProfit={setCustomBusinessProfit}
+      />
       <Commissions
         form={form}
         basePrice={basePrice}
