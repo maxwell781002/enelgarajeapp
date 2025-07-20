e8a10404428b9c3c90fdbd0921270daa6888bc3e
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2025-07-20 16:59:09 -0400

    restore home code

--- apps/backoffice/app/admin/dashboard/page.tsx
+++ apps/backoffice/app/admin/dashboard/page.tsx
@@ -1,48 +1,5 @@
-import { BtnServerAction } from "@repo/ui/components/btn-server-action";
 import { redirect } from "next/navigation";
-import {
-  simpleAction,
-  actionObject,
-  actionFormData,
-  withLogging,
-} from "@repo/model/repository/to-test-middleware";
-import { ZodError } from "zod";
 
 export default function Page() {
-  const objectFn = async () => {
-    "use server";
-    try {
-      await actionObject({ name: "test", a: 1 });
-    } catch (error: any) {
-      if (error instanceof ZodError) {
-        console.log("error fff", error.errors);
-      }
-    }
-  };
-  const formDataFn = async () => {
-    "use server";
-    try {
-      const formData = new FormData();
-      formData.append("name", "test");
-      await actionFormData(formData);
-    } catch (error: any) {
-      if (error instanceof ZodError) {
-        console.log("error fff", error.errors);
-      }
-    }
-  };
-  const loggingFn = async () => {
-    "use server";
-    const result = await withLogging();
-    console.log("result", result);
-  };
-  return (
-    <div>
-      <BtnServerAction action={simpleAction}>Simple Action</BtnServerAction>
-      <BtnServerAction action={objectFn}>Object</BtnServerAction>
-      <BtnServerAction action={formDataFn}>Object formData</BtnServerAction>
-      <BtnServerAction action={loggingFn}>Logging</BtnServerAction>
-    </div>
-  );
-  // return redirect("/admin/business");
+  return redirect("/admin/business");
 }
