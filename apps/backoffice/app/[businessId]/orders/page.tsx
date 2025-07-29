b489d2fcb8e7eb282a174cd1a9ab52faa8cf24b5
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2025-07-28 20:55:56 -0400

    Show the columns wholesale

--- apps/backoffice/app/[businessId]/orders/page.tsx
+++ apps/backoffice/app/[businessId]/orders/page.tsx
@@ -1,13 +1,14 @@
 import MyTable from "@repo/ui/components/table/index";
 import { crud } from "@repo/model/lib/crud";
 import { orderRepository } from "@repo/model/repositories/order";
-import { columns } from "./columns";
+import { getColumns } from "./columns";
 import Filter from "./filters";
 import { redirect } from "next/navigation";
 import { PaginationResult } from "@repo/model/types/pagination";
 import TableLayout from "@repo/ui/components/table-layout/layout";
 import { getTranslations } from "next-intl/server";
 import { TableContextProvider } from "@repo/ui/context/table";
+import { getBusinessById } from "packages/model/repository/business";
 
 type PageProps = {
   searchParams: Promise<any>;
@@ -28,7 +29,9 @@ export default async function Page({ params, searchParams }: PageProps) {
     const url = await search(query);
     return redirect(url);
   };
+  const business = await getBusinessById(businessId);
   const data = await list({ businessId });
+  const columns = getColumns(business?.hasWholesaleMode);
   return (
     <TableContextProvider>
       <TableLayout
