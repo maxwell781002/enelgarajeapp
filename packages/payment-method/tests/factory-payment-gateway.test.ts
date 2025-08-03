97b902301ca79bcc98456b68bcb88acd5395444c
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2025-08-03 19:39:16 -0400

    fix test

--- packages/payment-method/tests/factory-payment-gateway.test.ts
+++ packages/payment-method/tests/factory-payment-gateway.test.ts
@@ -1,7 +1,14 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { createPaymentGateway } from "../src/factory-payment-gateway";
 import { QvapayGateway, TropipayGateway } from "../src/payment-gateway";
 import { PaymentGatewayType } from "@repo/model/types/enums";
+
+const mocksAuth = vi.hoisted(() => ({
+  auth: vi.fn(() => ({})),
+}));
+vi.mock("@repo/model/lib/auth", () => ({
+  auth: mocksAuth.auth,
+}));
 
 describe("Gateway factory", () => {
   it("should create a tropipay gateway", () => {
